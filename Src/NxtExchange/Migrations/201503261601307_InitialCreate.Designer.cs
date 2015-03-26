532b524e0f9fc3fbc1193e87b19f67b0a8c34a8c
Liberty Swede <libertyswede@outlook.com>
2015-03-26 17:01:51 +0100

    Fixed naming

--- Src/NxtExchange/Migrations/201503261601307_InitialCreate.Designer.cs
+++ Src/NxtExchange/Migrations/201503261601307_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace NxtExchange.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class initialcreate : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(initialcreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "201503261559248_initialcreate"; }
+            get { return "201503261601307_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
--- Src/NxtExchange/Migrations/201503261601307_InitialCreate.cs
+++ Src/NxtExchange/Migrations/201503261601307_InitialCreate.cs
@@ -3,7 +3,7 @@ namespace NxtExchange.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class initialcreate : DbMigration
+    public partial class InitialCreate : DbMigration
     {
         public override void Up()
         {
